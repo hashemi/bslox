02c18b848723d735bc100f2972e0dd21db6aa4ed
Ahmad Alhashemi <ahmad@ahmadh.com>
2018-02-19 18:56:07 -0500

    Chapter 14. Chunks of Bytecode

---
+++ README.md
@@ -0,0 +1,20 @@
+# bslox
+
+This is a bytecode interpreter of the Lox language written in Swift.
+
+This project follows Bob Nystrom's excellent book, [Crafting Interpreters](http://www.craftinginterpreters.com) which takes you through the process of writing an interpreter for a language called Lox.
+
+The book describes two implementations. The first in Java and is a tree-walking interpreter. I have already ported that version to Swift in my other project, [slox](https://github.com/hashemi/slox). The final part of the book describes a bytecode interpreter in C. This is my Swift port of the bytecode interpreter.
+
+The book is being released as chapters are completed, one chapter at a time.
+
+## Progress
+As of February 19th, 2018, this version is up to date with Part III of the book. It implements the following chapters:
+
+14. Chunks of Bytecode.
+
+## Goals & Design
+As with my other port, I will sometimes deviate from the C implementation to take advantage of Swift's features and idioms. For example, instead of storing a constant's index inline with the bytecode, it's stored as an associated value. This may lead to less compact bytecode array. I will revisit this decision as we progress through other chapters.
+
+## License
+MIT
--- bslox/main.swift
+++ bslox/main.swift
@@ -8,5 +8,83 @@
 
 import Foundation
 
-print("Hello, World!")
+enum OpCode {
+    case `return`
+    case constant(index: UInt8)
+}
 
+struct Chunk {
+    var codes: [OpCode] = []
+    var lines: [Int] = []
+    var constants = ValueArray()
+
+    mutating func write(_ op: OpCode, line: Int) {
+        codes.append(op)
+        lines.append(line)
+    }
+    
+    mutating func addConstant(_ value: Value) -> UInt8 {
+        constants.write(value)
+        return UInt8(constants.count - 1)
+    }
+    
+    func disassemble(name: String) -> String {
+        return "== \(name) ==\n" +
+            (0..<codes.count).map {
+                String(format: "%04d ", $0) + disassemble(offset: $0)
+            }.joined(separator: "\n")
+    }
+    
+    func disassemble(offset: Int) -> String {
+        let op = codes[offset]
+        
+        var result: String
+        if (offset > 0 && lines[offset] == lines[offset - 1]) {
+            result = "   | "
+        } else {
+            result = String(format: "%4d ", lines[offset])
+        }
+        
+        switch op {
+        case .return:
+            result += "OP_RETURN"
+        case .constant(let constant):
+            result += String(format: "%-16@ %4d '", "OP_CONSTANT", constant)
+                + constants.values[Int(constant)].description
+                + "'"
+        }
+        
+        return result
+    }
+}
+
+struct Value: ExpressibleByFloatLiteral, CustomStringConvertible {
+    private let value: Double
+    
+    init(floatLiteral: Double) {
+        self.value = floatLiteral
+    }
+    
+    var description: String {
+        return String(format: "%g", value)
+    }
+}
+
+struct ValueArray {
+    var values: [Value] = []
+    
+    var count: Int { return values.count }
+    
+    mutating func write(_ value: Value) {
+        values.append(value)
+    }
+}
+
+var chunk = Chunk()
+
+let constant = chunk.addConstant(1.2)
+chunk.write(.constant(index: constant), line: 123)
+
+chunk.write(.return, line: 123)
+
+print(chunk.disassemble(name: "test chunk"))
